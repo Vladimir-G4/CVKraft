77188723356e43536ca16116b435a04643bb879c
jll38 <jll38@njit.edu>
2023-01-15 00:27:13 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+import {
+  Button,
+  Container,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  FormInput,
+  Collapse
+} from "shards-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar type="dark" theme="dark" expand="md">
+        <NavbarBrand>CVK</NavbarBrand>
+       <Nav navbar>
+       <NavItem>
+          <NavLink active href="/">Resume Builder</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink active href="/Privacy">Privacy</NavLink>
+        </NavItem>
+       </Nav>
+      </Navbar>
+      <Container id="quiz" fluid={true}>
+        <div id="starter">
+        <img src="static/images/CVKraft.jpg" id="cvk-logo" alt="CVKraft Logo"></img>
+        <Button outline theme="info">Get Started</Button>
+        </div>
+        
+      </Container>
     </div>
   );
 }
